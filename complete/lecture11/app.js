8aaeb5b53b33b0e8e2d7e83b06b2472baae3b1cd
pmtoliveira <pmtoliveira@hotmail.com>
2021-08-04 15:04:20 +0100

    Update app.js

--- complete/lecture11/app.js
+++ complete/lecture11/app.js
@@ -25,17 +25,19 @@ class App{
         ambient.position.set( 0.5, 1, 0.25 );
 		this.scene.add(ambient);
         
-        // const light = new THREE.DirectionalLight( 0xFFFFFF, 1.5 );
-        // light.position.set( 0.2, 1, 1);
-        // this.scene.add(light);
+        const light = new THREE.DirectionalLight( 0xFFFFFF, 1.5 );
+        light.position.set( 0.2, 1, 1);
+        this.scene.add(light);
 			
 		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
         this.renderer.outputEncoding = THREE.sRGBEncoding;
-        //this.renderer.physicallyCorrectLights = true;
+        this.renderer.physicallyCorrectLights = true;
         container.appendChild( this.renderer.domElement );
 		this.setEnvironment();
+        
+        this.workingVec3 = new THREE.Vector3();
 
         this.reticle = new THREE.Mesh(
             new THREE.RingBufferGeometry( 0.15, 0.2, 32 ).rotateX( - Math.PI / 2 ),
@@ -46,7 +48,7 @@ class App{
         this.reticle.visible = false;
         this.scene.add( this.reticle );
         
-        this.setupXR();
+        //this.setupXR();
 		
         //this.loadingBar = new LoadingBar();
         
