a7cf57af51039e32ef235112b3b92b8ac2e5d32d
pmtoliveira <pmtoliveira@hotmail.com>
2021-08-06 15:23:38 +0100

    Update app.js

--- complete/lecture11/app.js
+++ complete/lecture11/app.js
@@ -6,7 +6,10 @@ import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
 import { ARButton } from '../../libs/ARButton.js';
 import { LoadingBar } from '../../libs/LoadingBar.js';
 
+var browser = require("webextension-polyfill");
+
 class App{
+    
 	constructor(){
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
